22f4eae9e1be78e2bb78c434bb92f3e067e6fb66
Mark Goodwin <mgoodwin@mozilla.com>
2018-11-01 15:30:25 +0000

    Port from sha-2 to mmh3 based filter for perf, simplicity and tooling compat

--- src/lib.rs
+++ src/lib.rs
@@ -4,65 +4,59 @@
 extern crate bit_vec;
 extern crate byteorder;
 extern crate digest;
+extern crate murmurhash3;
 extern crate rand;
-extern crate sha2;
 
 use byteorder::{BigEndian, ByteOrder, ReadBytesExt};
 use bit_vec::BitVec;
+use murmurhash3::murmurhash3_x86_32;
 
 use std::str;
 
-pub trait BloomHasher {
-    fn bloom_input(&mut self, data: &[u8]);
-    fn bloom_result(&mut self) -> Vec<u8>;
-}
-
-pub trait HasherMaker {
-    fn make_hasher(&self) -> Box<BloomHasher>;
-}
-
-pub struct Bloom<'a> {
+pub struct Bloom {
+    level: u32,
+    elements: u32,
+    false_positive_rate: f32,
+    n_hash_funcs: u32,
+    size: usize,
     bitvec: BitVec,
-    k: u32,
-    bits: usize,
-    hasher_maker: &'a HasherMaker
 }
 
-impl<'a> Bloom<'a> {
-    pub fn new(size: usize, k: u32, hasher_maker: &'a HasherMaker) -> Bloom {
-        assert!(size > 0);
+impl Bloom {
+    pub fn new(elements: u32, false_positive_rate: f32, level: u32) -> Bloom {
 
-        let bits = size * 8usize;
-        let bitvec = BitVec::from_elem(bits as usize, false);
-        let k = k;
+        let n_hash_funcs = ((1.0 / false_positive_rate).ln() / (2.0_f32).ln()).ceil() as u32;
+        let hashes = n_hash_funcs as f32;
+        let size = (1.0_f32 - (hashes * (elements as f32 + 0.5) / (1.0_f32 - false_positive_rate.powf(1.0 / hashes)).ln())).ceil() as usize;
+
+        let bitvec = BitVec::from_elem(size as usize, false);
 
         Bloom {
+            level: level,
+            elements: elements,
+            false_positive_rate: false_positive_rate,
+            n_hash_funcs: n_hash_funcs,
+            size: size,
             bitvec: bitvec,
-            k: k,
-            bits: bits,
-            hasher_maker: hasher_maker
         }
     }
 
+    fn hash(&self, n_fn: u32, key: &[u8]) -> usize {
+        let hash_seed = (n_fn << 16) + self.level;
+        let h = murmurhash3_x86_32(key, hash_seed);
+        h as usize % self.size
+    }
+
     pub fn put(&mut self, item: &[u8]) {
-        for i in 0..self.k {
-            let mut hash = self.hasher_maker.make_hasher();
-            let mut buf = [0; 4];
-            BigEndian::write_u32(&mut buf, i);
-            hash.bloom_input(&buf);
-            hash.bloom_input(item);
-            self.bitvec.set(BigEndian::read_u32(&hash.bloom_result()) as usize % self.bits, true);
+        for i in 0..self.n_hash_funcs {
+            let index = self.hash(i, item);
+            self.bitvec.set(index, true);
         }
     }
 
     pub fn has(&self, item: &[u8]) -> bool {
-        for i in 0..self.k {
-            let mut hash = self.hasher_maker.make_hasher();
-            let mut buf = [0; 4];
-            BigEndian::write_u32(&mut buf, i);
-            hash.bloom_input(&buf);
-            hash.bloom_input(item);
-            if  self.bitvec.get(BigEndian::read_u32(&hash.bloom_result()) as usize % self.bits).unwrap() == false {
+        for i in 0..self.n_hash_funcs {
+            if  self.bitvec.get(self.hash(i, item)).unwrap() == false {
                 return false;
             }
         }
@@ -75,25 +69,26 @@ impl<'a> Bloom<'a> {
     }
 }
 
-pub struct Cascade<'a> {
-    filter: Bloom<'a>,
-    child_layer: Option<Box<Cascade<'a>>>,
-    depth: usize,
-    hasher_maker: &'a HasherMaker
+pub struct Cascade {
+    filter: Bloom,
+    child_layer: Option<Box<Cascade>>,
+    depth: u32,
+    error_rate: f32,
+    oversize_factor: f32,
 }
 
-impl<'a> Cascade<'a> {
-    pub fn new(capacity: usize, hasher_maker: &'a HasherMaker) -> Cascade<'a> {
-        return Cascade::new_layer(capacity, 0, hasher_maker);
+impl Cascade {
+    pub fn new(capacity: usize, oversize_factor: f32, error_rate: f32) -> Cascade {
+        return Cascade::new_layer(capacity, oversize_factor, error_rate, 0);
     }
 
-    fn new_layer(capacity: usize, depth: usize, hasher_maker: &'a HasherMaker) -> Cascade<'a> {
+    fn new_layer(capacity: usize, oversize_factor: f32, error_rate: f32, depth: u32) -> Cascade {
         Cascade {
-            // TODO: MDG calculate k based on error rate - hardcoded for groovecoder's example
-            filter: Bloom::new(capacity, 2, hasher_maker),
+            filter: Bloom::new((capacity as f32 * oversize_factor) as u32, 0.5, depth),
             child_layer: Option::None,
             depth: depth,
-            hasher_maker: hasher_maker
+            error_rate: error_rate,
+            oversize_factor: oversize_factor
         }
     }
     pub fn initialize(&mut self, entries: Vec<Vec<u8>>, exclusions: Vec<Vec<u8>>) {
@@ -109,41 +104,18 @@ impl<'a> Cascade<'a> {
         }
 
         if false_positives.len() > 0 {
-            let mut mangled_false_positives = Vec::new();
-            let mut mangled_entries = Vec::new();
-            println!("New layer for {} false positives", false_positives.len());
-            
-            for entry in false_positives {
-                let mut v = entry.to_vec();
-                if self.depth > 0 {
-                    v.push(65);
-                }
-                mangled_false_positives.push(v);
-            }
-
-            for entry in entries {
-                let mut v = entry.to_vec();
-                if self.depth > 0 {
-                    v.push(65);
-                }
-                mangled_entries.push(v);
-            }
             let mut child = Box::new(
-                Cascade::new_layer(mangled_false_positives.len(), self.depth + 1, self.hasher_maker));
-            child.initialize(mangled_false_positives, mangled_entries);
+                Cascade::new_layer(false_positives.len(), self.oversize_factor, self.error_rate, self.depth + 1));
+            child.initialize(false_positives, entries);
             self.child_layer = Some(child);
         }
     }
 
     pub fn has(&self, entry: Vec<u8>) -> bool {
         if self.filter.has(&entry) {
-            let mut mangled_entry = entry.to_vec();
-            if self.depth > 0 {
-                mangled_entry.push(65);
-            }
             match self.child_layer {
                 Some(ref child) => {
-                    return ! child.has(mangled_entry);
+                    return ! child.has(entry);
                 },
                 None => {
                     return true;
@@ -170,46 +142,21 @@ impl<'a> Cascade<'a> {
     }
 }
 
-// construct a BloomHasher and HasherMaker for tests
-use sha2::{Sha256, Digest};
-
-// Implement the BloomHasher trait for sha2::Sha256 - we could also do this for the hash types used by
-// the standard collections
-impl BloomHasher for Sha256 {
-    fn bloom_input(&mut self, data: &[u8]) {
-        self.input(data)
-    }
-
-    fn bloom_result(&mut self) -> Vec<u8> {
-        self.clone().result().to_vec()
-    }
-}
-
-struct Sha256Maker {}
-impl HasherMaker for Sha256Maker {
-    fn make_hasher(&self) -> Box<BloomHasher> {
-        Box::new(Sha256::default())
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use Bloom;
     use Cascade;
     use rand::prelude::*;
-    use Sha256Maker;
 
     #[test]
     fn bloom_test_bloom_size() {
-        let maker = Sha256Maker{};
-        let bloom = Bloom::new(1024, 2, &maker);
-        assert!(bloom.bitvec.len() == 8192);
+        let bloom = Bloom::new(1024, 0.01, 0);
+        assert!(bloom.bitvec.len() == 9829);
     }
 
     #[test]
     fn bloom_test_put() {
-        let maker = Sha256Maker {};
-        let mut bloom = Bloom::new(1024, 2, &maker);
+        let mut bloom = Bloom::new(1024, 0.01, 0);
         let key: &[u8] = b"foo";
 
         bloom.put(key);
@@ -217,8 +164,7 @@ mod tests {
 
     #[test]
     fn bloom_test_has() {
-        let maker = Sha256Maker {};
-        let mut bloom = Bloom::new(1024, 2, &maker);
+        let mut bloom = Bloom::new(1024, 0.01, 0);
         let key: &[u8] = b"foo";
 
         bloom.put(key);
@@ -246,8 +192,7 @@ mod tests {
             bar.push(foo.swap_remove(idx));
         }
 
-        let maker = Sha256Maker {};
-        let mut cascade = Cascade::new(500, &maker);
+        let mut cascade = Cascade::new(500, 1.1, 0.5);
         cascade.initialize(foo.clone(), bar.clone());
 
         assert!(cascade.check(foo.clone(), bar.clone()) == true);
